60d71dad133111efe57656134bebd820a25e5f76
Nat Dunn <ndunn219@users.noreply.github.com>
2017-06-06 09:18:18 +0200

    Fixing Race Condition

--- pyt211.py
+++ pyt211.py
@@ -6,5 +6,6 @@ file_name = 'classfiles.zip'
 
 with urllib.request.urlopen(url) as response, open(file_name, 'wb') as out_file:
     shutil.copyfileobj(response, out_file)
-    with zipfile.ZipFile(file_name) as zf:
-        zf.extractall()
+
+with zipfile.ZipFile(file_name) as zf:
+    zf.extractall()
