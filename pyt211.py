a4551867b2adcee46a39d96679ac7355be232e24
Nat Dunn <ndunn219@users.noreply.github.com>
2018-11-07 05:46:45 -0500

    Update pyt211.py
    Dealing with having created different class files for course delivery versions.

--- pyt211.py
+++ pyt211.py
@@ -1,7 +1,12 @@
 import zipfile, urllib.request, shutil, os
 
-courseid = input('Course ID: ')
-url = 'https://www.webucator.com/ClassFiles/getClassFiles.cfm?CourseID=' + courseid
+course_id = input('Course ID: ').upper()
+if course_id == 'PYT211':
+    url = 'https://www.webucator.com/class-files/index.cfm?versionId=3894'
+elif course_id == 'PYT251':
+    url = 'https://www.webucator.com/class-files/index.cfm?versionId=3804'
+else:
+    url = 'https://www.webucator.com/ClassFiles/getClassFiles.cfm?CourseID=' + course_id
 file_name = 'classfiles.zip'
 
 with urllib.request.urlopen(url) as response, open(file_name, 'wb') as out_file:
