8c8910d21b8ace46d3510135683c6b9e8e037208
Nat Dunn <ndunn219@users.noreply.github.com>
2019-04-02 14:49:37 -0400

    Updated to versionid 4283 for PYT211

--- pyt211.py
+++ pyt211.py
@@ -2,7 +2,7 @@ import zipfile, urllib.request, shutil, os
 
 course_id = input('Course ID: ').upper()
 if course_id == 'PYT211':
-    url = 'https://www.webucator.com/class-files/index.cfm?versionId=3894'
+    url = 'https://www.webucator.com/class-files/index.cfm?versionId=4283'
 elif course_id == 'PYT251':
     url = 'https://www.webucator.com/class-files/index.cfm?versionId=3804'
 else:
