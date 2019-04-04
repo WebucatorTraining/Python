b5a98b88b1f8a61bb77abc57d3f50738bc1ed61c
Nat Dunn <ndunn219@users.noreply.github.com>
2019-04-04 12:06:18 -0400

    Update and rename pyt211.py to python_class_files_sp.py
    Using type parameter in URL. Changed file name.

--- python_class_files_sp.py
+++ python_class_files_sp.py
@@ -1,12 +1,7 @@
 import zipfile, urllib.request, shutil, os
 
 course_id = input('Course ID: ').upper()
-if course_id == 'PYT211':
-    url = 'https://www.webucator.com/class-files/index.cfm?versionId=4283'
-elif course_id == 'PYT251':
-    url = 'https://www.webucator.com/class-files/index.cfm?versionId=3804'
-else:
-    url = 'https://www.webucator.com/ClassFiles/getClassFiles.cfm?CourseID=' + course_id
+url = 'https://www.webucator.com/ClassFiles/getClassFiles.cfm?Type=SP&CourseID=' + course_id
 file_name = 'classfiles.zip'
 
 with urllib.request.urlopen(url) as response, open(file_name, 'wb') as out_file:
