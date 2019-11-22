2743ad0aa0f1fc2b64763809ce5cb0a74919b099
Nat Dunn <ndunn219@users.noreply.github.com>
2019-11-22 18:08:05 -0500

    Copy of python_class_files_sp.py
    This is an exact copy of python_class_files_sp.py, because we have a bit.ly (http://bit.ly/pyt211-py) that points to this URL in the PYT211 SP course.

---
+++ pyt211.py
@@ -0,0 +1,11 @@
+import zipfile, urllib.request, shutil, os
+
+course_id = input('Course ID: ').upper()
+url = 'https://www.webucator.com/ClassFiles/getClassFiles.cfm?Type=SP&CourseID=' + course_id
+file_name = 'classfiles.zip'
+
+with urllib.request.urlopen(url) as response, open(file_name, 'wb') as out_file:
+    shutil.copyfileobj(response, out_file)
+
+with zipfile.ZipFile(file_name) as zf:
+    zf.extractall()
